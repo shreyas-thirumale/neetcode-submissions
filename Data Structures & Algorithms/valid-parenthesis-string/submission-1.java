e07faeae70848a231dddc07f93ea3e7bd87d79c8
shreyas-thirumale <shreyas.thirumale25@gmail.com>
2026-07-11 16:42:10 -0400

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') stack.push(i);
+            else if (c == '*') star.push(i);
+            else {
+                if (!stack.isEmpty()) stack.pop();
+                else if (!star.isEmpty()) star.pop();
+                else return false;
+            }
+        }
+        while (!stack.isEmpty() && !star.isEmpty()) {
+            if (stack.peek() > star.peek()) return false;
+            stack.pop();
+            star.pop();
+        }
+        return stack.isEmpty();
+        
+    }
+}
