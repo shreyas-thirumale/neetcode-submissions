5b840d1b61fb9391b583e37539a3be52e2b0ebd1
Shreyas <shreyas.thirumale25@gmail.com>
2026-07-23 16:19:19 -0400

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) return false;
+        Arrays.sort(hand);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < hand.length; i++) {
+            map.put(hand[i], map.getOrDefault(hand[i], 0) + 1); 
+        }
+        for (int num : hand) {
+            if (!map.containsKey(num)) continue;
+            map.put(num, map.get(num) - 1);
+            if (map.get(num) == 0) map.remove(num);
+            int next = num + 1;
+            for (int i = 1; i < groupSize; i++) {
+                if (!map.containsKey(next)) return false;
+                map.put(next, map.get(next) - 1);
+                if (map.get(next) == 0) map.remove(next);
+                next++;
+            }
+        }
+        return true;
+    }
+}
